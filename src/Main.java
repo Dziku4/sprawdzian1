bc2d6324a96af220d1852cefbc9787a81f45d501
Nikodem <2009nikodemmmarek@gmail.com>
2024-10-21 11:18:24 +0200

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wpisz imię i nazwisko");
+        System.out.println("Nikodem");
         String name = scanner.nextLine();
-        System.out.println("Podaj wiek");
+        System.out.println("15");
         int age = scanner.nextInt();
         System.out.println("Witaj " + name + ". Za " + (18 - age) + " lata będę miał 18 lat!");
 //        na branchu 'twoje imię' w następnej linii wydrukuj pierwszą rzecz jaką chcesz robić po uzyskaniu pełnoletności
+        System.out.println("Kupić samochód");
     }
 }
